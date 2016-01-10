3a5489e3b39f1d7a087f7ba0d623d44b036ddb04
Simon Forsberg <zomis2k@hotmail.com>
2016-01-10 11:31:56 +0100

    Extract method in `LinearRegressionTest`

--- src/test/groovy/net/zomis/regression/LinearRegressionTest.groovy
+++ src/test/groovy/net/zomis/regression/LinearRegressionTest.groovy
@@ -6,12 +6,16 @@ import org.junit.Test
 
 class LinearRegressionTest {
 
+    static double[] gradientDescent(double[][] xs, double[] y, int thetaParameters) {
+        GradientDescent.gradientDescent(LinearRegression.costFunction(xs, y),
+                new ConvergenceIterations(10000), new double[thetaParameters], 0.01)
+    }
+
     @Test(expected = IllegalArgumentException)
     void requiresParameters() {
         double[][] xs = [[1] as double[], [2] as double[], [3] as double[], [4] as double[]] as double[][]
         double[] y = [2, 4, 5, 8] as double[]
-        GradientDescent.gradientDescent(LinearRegression.costFunction(xs, y),
-                new ConvergenceIterations(10000), new double[0], 0.01)
+        gradientDescent(xs, y, 0)
         assert false
     }
 
@@ -19,8 +23,7 @@ class LinearRegressionTest {
     void requiresMatchingParamSize() {
         double[][] xs = [[1] as double[], [2] as double[], [3] as double[], [4] as double[]] as double[][]
         double[] y = [2, 4, 5, 8] as double[]
-        GradientDescent.gradientDescent(LinearRegression.costFunction(xs, y),
-                new ConvergenceIterations(10000), new double[1], 0.01)
+        gradientDescent(xs, y, 1)
         assert false
     }
 
@@ -28,8 +31,7 @@ class LinearRegressionTest {
     void simple1_9Line() {
         double[][] xs = [[1] as double[], [2] as double[], [3] as double[], [4] as double[]] as double[][]
         double[] y = [2, 4, 5, 8] as double[]
-        double[] theta = GradientDescent.gradientDescent(LinearRegression.costFunction(xs, y),
-            new ConvergenceIterations(10000), new double[2], 0.01)
+        double[] theta = gradientDescent(xs, y, 2)
         assert doubleEquals(theta, [0, 1.9] as double[])
     }
 
