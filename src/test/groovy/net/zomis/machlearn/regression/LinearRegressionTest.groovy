3ca8facd22ca6cd381874f1520d03ff85f58fc34
Simon Forsberg <zomis2k@hotmail.com>
2016-01-11 19:28:33 +0100

    Extract LinearRegressionTest training set

--- src/test/groovy/net/zomis/machlearn/regression/LinearRegressionTest.groovy
+++ src/test/groovy/net/zomis/machlearn/regression/LinearRegressionTest.groovy
@@ -9,27 +9,23 @@ class LinearRegressionTest {
         GradientDescent.gradientDescent(LinearRegression.costFunction(xs, y),
                 new ConvergenceIterations(10000), new double[thetaParameters], 0.01)
     }
+    double[][] xs = [[1] as double[], [2] as double[], [3] as double[], [4] as double[]] as double[][]
+    double[] y = [2, 4, 5, 8] as double[]
 
     @Test(expected = IllegalArgumentException)
     void requiresParameters() {
-        double[][] xs = [[1] as double[], [2] as double[], [3] as double[], [4] as double[]] as double[][]
-        double[] y = [2, 4, 5, 8] as double[]
         gradientDescent(xs, y, 0)
         assert false
     }
 
     @Test(expected = IllegalArgumentException)
     void requiresMatchingParamSize() {
-        double[][] xs = [[1] as double[], [2] as double[], [3] as double[], [4] as double[]] as double[][]
-        double[] y = [2, 4, 5, 8] as double[]
         gradientDescent(xs, y, 1)
         assert false
     }
 
     @Test
     void simple1_9Line() {
-        double[][] xs = [[1] as double[], [2] as double[], [3] as double[], [4] as double[]] as double[][]
-        double[] y = [2, 4, 5, 8] as double[]
         double[] theta = gradientDescent(xs, y, 2)
         assert TestHelp.doubleEquals(theta, [0, 1.9] as double[])
     }
