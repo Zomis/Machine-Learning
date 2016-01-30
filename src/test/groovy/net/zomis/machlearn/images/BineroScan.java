c944438679948b13cc479df0f93d9da43021e2c3
Simon Forsberg <zomis2k@hotmail.com>
2016-01-30 22:28:29 +0100

    Added some calculations for finding the boxes in Binero, WIP

--- src/test/groovy/net/zomis/machlearn/images/BineroScan.java
+++ src/test/groovy/net/zomis/machlearn/images/BineroScan.java
@@ -67,6 +67,56 @@ public class BineroScan {
     }
 
     public static ZRect[][] createRectsFromLines(List<Integer> xSeparatorLines, List<Integer> ySeparatorLines) {
+        int[] values = xSeparatorLines.stream().mapToInt(i -> i).toArray();
+        int[] diffsX = new int[xSeparatorLines.size()];
+        int old = 0;
+        for (int i = 0; i < xSeparatorLines.size(); i++) {
+            int value = xSeparatorLines.get(i);
+            diffsX[i] = value - old;
+            old = value;
+        }
+        System.out.println(xSeparatorLines);
+        System.out.println(Arrays.toString(diffsX));
+        int[] sortedX = Arrays.copyOf(diffsX, diffsX.length);
+        Arrays.sort(sortedX);
+        int squareSize = 0;
+        for (int value : sortedX) {
+            squareSize = value;
+            if (squareSize > 1) {
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(sortedX));
+        System.out.println(squareSize);
+        int firstIndex = 0;
+        int lastIndex = diffsX.length - 1;
+        while (Math.abs(diffsX[firstIndex] - squareSize) >= 2) firstIndex++;
+        while (Math.abs(diffsX[lastIndex] - squareSize) >= 2) lastIndex--;
+        System.out.println(Arrays.toString(Arrays.copyOfRange(values, firstIndex - 1, lastIndex + 1)));
+
+        List<Integer> result = new ArrayList<>();
+        System.out.println("Add F: " + xSeparatorLines.get(firstIndex - 1));
+        result.add(xSeparatorLines.get(firstIndex - 1));
+        for (int i = firstIndex; i <= lastIndex; i++) {
+            if (diffsX[i] < squareSize) {
+                continue;
+            }
+            int a = xSeparatorLines.get(i);
+            System.out.println("Add A: " + a);
+            result.add(a);
+            int skip = 0;
+            while (diffsX[i + skip + 1] < squareSize) {
+                skip++;
+            }
+            int b = xSeparatorLines.get(i + skip);
+            System.out.println("Add B: " + b);
+            result.add(b);
+            i += skip;
+            int diff = diffsX[i];
+            System.out.println(diff);
+        }
+        System.out.println(result);
+
         return new ZRect[0][];
     }
 
