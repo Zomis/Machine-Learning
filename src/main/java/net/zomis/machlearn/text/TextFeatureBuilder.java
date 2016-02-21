25100265c665e3cb1306358426296e83e99753a0
Simon Forsberg <zomis2k@hotmail.com>
2016-02-21 14:30:56 +0100

    Add NO-OP TextFeatureBuilder

---
+++ src/main/java/net/zomis/machlearn/text/TextFeatureBuilder.java
@@ -0,0 +1,14 @@
+package net.zomis.machlearn.text;
+
+public class TextFeatureBuilder {
+
+
+    public void add(String processed) {
+
+    }
+
+    public TextFeatureMapper mapper() {
+        return new TextFeatureMapper();
+    }
+
+}
