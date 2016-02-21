9c74cf3db6b94bf6941f2871a85ef956ae21c5a2
Simon Forsberg <zomis2k@hotmail.com>
2016-02-21 14:26:51 +0100

    Add the possibility to pass object represented to LearningData, for better toString

--- src/main/java/net/zomis/machlearn/neural/LearningData.java
+++ src/main/java/net/zomis/machlearn/neural/LearningData.java
@@ -7,12 +7,22 @@ public class LearningData {
     public final double[] inputs;
     public final double[] outputs;
     public final double weight;
+    private final Object forData;
 
     public LearningData(double[] inputs, double[] outputs) {
         this(inputs, outputs, 1);
     }
 
+    public LearningData(Object forData, double[] inputs, double[] outputs) {
+        this(forData, inputs, outputs, 1);
+    }
+
     public LearningData(double[] inputs, double[] outputs, double weight) {
+        this(null, inputs, outputs, weight);
+    }
+
+    public LearningData(Object forData, double[] inputs, double[] outputs, double weight) {
+        this.forData = forData;
         this.inputs = inputs;
         this.outputs = outputs;
         this.weight = weight;
@@ -27,7 +37,7 @@ public class LearningData {
         return "LearningData{" +
                 "inputs=" + Arrays.toString(inputs) +
                 ", outputs=" + Arrays.toString(outputs) +
-                '}';
+                '}' + (forData == null ? "" : " " + forData);
     }
 
     public double[] getOutputs() {
